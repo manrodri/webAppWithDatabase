a6df83b6c8d3fe5ab9b077a9e2f30ebea094d9f9
Manuel Rodriguez <manuel.rodriguez@ecs-digital.co.uk>
2019-07-03 11:39:34 +0100

    python script to kill and start node app added

--- run.py
+++ run.py
@@ -67,6 +67,7 @@ else:
 
 # run the app
 cmd = "sudo nohup node {} > /tmp/yelpCamp.log &".format(args.entry_point)
+cmd = 'pwd && ls -alh'
 success, output, exit_code = execute_command_with_output(cmd, logger, message="Running release candiate")
 if success:
   logger.info('yelpCamp server started')
