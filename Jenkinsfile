e4c1785a7707be83e48d0457c4d3b8b0d54111f9
Manuel Rodriguez <manuel.rodriguez@ecs-digital.co.uk>
2019-07-03 11:24:15 +0100

    python script to kill and start node app added

--- Jenkinsfile
+++ Jenkinsfile
@@ -37,7 +37,7 @@ pipeline {
                                         //removePrefix: 'dist/',
                                         //remoteDirectory: '/tmp',
                                         execCommand: """cd /tmp && curl -uadmin:APkvALzx9a7Ygn2kQ17Bcn7BU4 -O http://54.72.240.241:8081/artifactory/generic-local/yelpCamp_${env.BUILD_NUMBER}.zip && \
-                                                        unzip yelpCamp_${env.BUILD_NUMBER}.zip -d /tmp/app_${env.BUILD_NUMBER} > /dev/null && python3 /tmp/app_${env.BUILD_NUMBER}/run.py
+                                                        unzip yelpCamp_${env.BUILD_NUMBER}.zip -d /tmp/app_${env.BUILD_NUMBER} > /dev/null && python3 /tmp/app_${env.BUILD_NUMBER}/run.py app.js 3000 
                                                         """
 
                                         //execCommand: 'if [[ -e /tmp/run.sh ]] ; then rm -f /tmp/run.sh;  fi &&  unzip /tmp/yelpCamp_run.zip -d /tmp &&  sh /tmp/run.sh && ps aux | grep node',
--- run.py
+++ run.py
@@ -1,66 +1,73 @@
 import os
 import sys
 import subprocess
+from subprocess import Popen, PIPE
 from zipfile import ZipFile
 import logging
+import argparse
 
-"""
+# build parser and parse arguments
+parser = argparse.ArgumentParser(description='command line tool to kill a previuous version and deploy new release candidate')
 
-print('killing node process...')
-os.system('sudo pkill node')
+parser.add_argument('entry_point', help='entry point for node application')
+parser.add_argument('port', help='port node application is running')
+parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+parser.add_argument('--debug', action='store_true')
+
+args = parser.parse_args()
 
 
+def set_up_logger():
+    # set logger
+    FORMAT = '[%(levelname)-2s] %(message)s'
+    logging.basicConfig(format=FORMAT, level=10)
+    logger = logging.getLogger()
+    return logger
 
-# # stop node app
-# echo 'running: $ killall node'
-# sudo pkill node
-# if [ $? -eq 0 ]; then
-#   echo 'node app killed'
-# fi
-# echo checking app is not running: ...
-# echo 'running $ ps aux | grep'
-# ps aux | grep node
 
-# check if folder exists
+def execute_command_with_output(command, log, message=None, show_output=True):
+    '''this function executes an unix command with output'''
+    if isinstance(command, list):
+        command = " ".join(command)
+    log.info('executing command: %s ' % command)
+    if message:
+        log.info(message)
+    proc = Popen(command, shell=True, stdout=PIPE)
+    exec_output, exec_err = proc.communicate()
+    if show_output:
+        log.info("%s\n%s" % (exec_output, exec_err))
+        print "%s" % exec_output
+    if proc.returncode == 0:
+        return True, exec_output, proc.returncode
+    else:
+        log.info('Error in execution of command: %s ', command)
+        return False, exec_err, proc.returncode
 
-app_folder ='/tmp/app'
-if os.path.exists(app_folder):
-  for afile in os.listdir(app_folder):
-    os.remove(afile)
+logger = set_up_logger()
+
+logger.info('Running from: {}'.format(os.getcwd()))
+
+# I need to check if yelpCamp app is running
+cmd = "netstat -ltnp | grep -w ':{}'".format(int(args.port))
+rc = execute_command_with_output(cmd, logger, message="Running: {}".format(cmd), show_output=True)
+if rc[1]:
+  lines = rc[1].split(os.linesep)
+  if rc[0]:
+    for line in lines:
+      words = line.split()
+      pid = words[-1].split('/')[0]
+      cmd = 'kill {}'.format(pid)
+      success, output, exit_code = execute_command_with_output(cmd , logger, message='Running: {}'.format(cmd))
+      if success:
+        logger.info('Pid {} killed'.format(pid))
+        break
+
 else:
-  os.mkdir('/tmp/app')
-
-
-file_name = os.path.join('/tmp', 'yelpCamp.zip')
-with ZipFile(file_name, 'r') as zip: 
-    # printing all the contents of the zip file 
-    zip.printdir() 
-    # extracting all the files 
-    print('Extracting ...') 
-    zip.extractall() 
-  
-
-
-# # unzip artifact
-# if  [ -e '/tmp/yelpCamp.zip' ]
-# then
-#   unzip  /tmp/yelpCamp.zip  -d /tmp/app > /dev/null
-#   if [ $? -ne 0 ]; then
-#     echo 'Error unzipping artifact'
-#     exit 4
-#   fi
-# else
-#   echo could not find release cantidate
-#   exit 3
-# fi
+  logger.error('No service running on port: {}'.format(int(port)))
 
 # run the app
-#cd /tmp/app
-cmd = ['nohup', 'node', 'app.js', '>', '/tmp/yelpCamp.log', '&']
-subprocess.run(cmd, shell=True)
-with open('/tmp/yelpCamp.log', 'r') as f:
-  for line in f:
-    print(line)
-#cat /tmp/yelpCamp.log
-"""
-print ('Running the app: GODD LUCK!')
+cmd = "nohup node {} > /tmp/yelpCamp.log &".format(args.entry_point)
+success, output, exit_code = execute_command_with_output(cmd, logger, message="Running release candiate")
+if success:
+  logger.info('yelpCamp server started')
+
