6790229992b067c03930fea70940b6c8ef836b38
Manuel Rodriguez <manuel.rodriguez@ecs-digital.co.uk>
2019-07-05 12:28:11 +0100

    testing deloying to terraform provisioned instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "staging_server" {
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0b0afa51d47dcad45"]
   subnet_id = "subnet-0f97221768fbbfa7c"
-  private_ip = "$(var.PRIVATE_IP)"
+  private_ip = "192.168.1.131"
 
   tags = {
     Name = "server deployed by Terraform"
