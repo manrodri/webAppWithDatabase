aaeac6182be89f29b7d91326534d228f85b96dec
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2019-07-05 23:39:57 +0100

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "staging_server" {
 
   ami = "ami-025ec0b13f60902a6"
   instance_type = "t2.micro"
+  key_name      = "manuel_tech_case"
   vpc_security_group_ids = ["sg-0b0afa51d47dcad45"]
   subnet_id = "subnet-0f97221768fbbfa7c"
   
