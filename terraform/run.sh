1e76c8b8280167841c74d7da7f150a95f0686b9c
Manuel Rodriguez <manuel.rodriguez@ecs-digital.co.uk>
2019-07-05 15:41:11 +0100

    testing deloying to terraform provisioned instance

--- terraform/run.sh
+++ terraform/run.sh
@@ -1,3 +1,3 @@
 cd /tmp && curl -uadmin:AP4yc6KiPJbd7q36GqhzhxVHzFB -O http://34.244.56.79:8081/artifactory/generic-local/yelpCamp.zip
-unzip yelpCamp.zip -d /tmp/app > /dev/null
-cd /tmp/app &&  nohup node app/app.js
+unzip yelpCamp.zip -d  > /dev/null
+cd /tmp/yelpCampApp &&  nohup node app/app.js
