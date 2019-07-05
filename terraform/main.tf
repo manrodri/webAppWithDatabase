f07736c2988bb7984f2edd05b097c7b0622ca1cb
Manuel Rodriguez <manuel.rodriguez@ecs-digital.co.uk>
2019-07-05 16:51:24 +0100

    testing deloying to terraform provisioned instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,10 @@ resource "aws_instance" "staging_server" {
     private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
   }
   
+
+  provisioner "local-exec"{
+    command = "echo ${aws_instance.staging_server.public_ip}"
+  }
   provisioner "file" {
     source      = "../run.py"
     destination = "/tmp/run.py"
