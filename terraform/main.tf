330e67221a436b393f05f6ceade98be6c10efd62
Manuel Rodriguez <manuel.rodriguez@ecs-digital.co.uk>
2019-07-05 16:49:23 +0100

    testing deloying to terraform provisioned instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,16 +9,11 @@ resource "aws_instance" "staging_server" {
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-0b0afa51d47dcad45"]
   subnet_id = "subnet-0f97221768fbbfa7c"
-  private_ip = "192.168.1.131"
   
   connection {
     type     = "ssh"
     user     = "jenkins"
     private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
-  }
-
-  provisioner "local-exec" {
-    command = "ls ../run.py"
   }
   
   provisioner "file" {
