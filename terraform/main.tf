32c0256903be42ec9d4c0332ddf19a86c4ae8056
Manuel Rodriguez <manuel.rodriguez@ecs-digital.co.uk>
2019-07-05 17:43:34 +0100

    changing entry point

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,7 +27,7 @@ resource "aws_instance" "staging_server" {
   provisioner "remote-exec" {
     inline = [
       "python2 /tmp/run.py 3000",
-      "sh /tmp/yelpCampApp/bin/www.sh",
+      "node /tmp/yelpCampApp/bin/www",
       "netstat -ltnp | grep -w 3000"
     ]
   }
