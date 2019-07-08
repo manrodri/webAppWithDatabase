18bd30a18a2dc7cd03431175246b6a2d3365bcdd
Manuel Rodriguez <manuel.rodriguez@ecs-digital.co.uk>
2019-07-08 16:22:38 +0200

    test e2e pipeline

--- run_smoke_test.py
+++ run_smoke_test.py
@@ -17,10 +17,8 @@ handler.setFormatter(formatter)
 
 if __name__ == '__main__':
 
-    pattern = sys.argv[1] if len(sys.argv) >=2 else "test*.py"
-    if pattern == 'all':
-        pattern = "test*.py"
-    test_dir = sys.argv[2] if len(sys.argv) >=3 else "smokeTest"
+    pattern =  "test*.py"
+    test_dir = "smokeTest"
 
     sys.path.insert(0, test_dir)
     print("Running {} within {}".format(pattern, test_dir))
