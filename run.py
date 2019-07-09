1e39f690bbe4ff123649f4d238157e4215a2c18a
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2019-07-09 21:55:25 +0100

    edit port mapping production deployment

--- run.py
+++ run.py
@@ -67,7 +67,7 @@ else:
 
 # retrieve artifact from artifactory
 os.chdir('/tmp')
-cmd = 'curl -uadmin:AP4yc6KiPJbd7q36GqhzhxVHzFB -O http://34.244.186.41:8081/artifactory/generic-local/yelpCamp.zip'
+cmd = "curl -uadmin:AP4yc6KiPJbd7q36GqhzhxVHzFB -O http://artifactory:8081/artifactory/generic-local/yelpCamp_${env.BUILD_NUMBER}.zip"
 rc = execute_command_with_output(cmd, logger, message='Retrieving artifact')
 if not rc[0]:
   logger.error(rc[1])
