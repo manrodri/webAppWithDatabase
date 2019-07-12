e992d6bfce979226efe6902e222d8bfb5f28c8ef
Manuel Rodriguez <manuel.rodriguez@ecs-digital.co.uk>
2019-07-12 16:40:45 +0100

    testing to chnge ansible hostfile

--- add_public_ip.py
+++ add_public_ip.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import logging
+import re
+
 
 def set_up_logger():
     # set logger
@@ -11,16 +13,26 @@ def set_up_logger():
 logger = set_up_logger()
 
 script_dir = os.getcwd()
-hosts_path = os.path.join(script_dir, sys.argv[1])
+hosts_path = os.path.join(script_dir, sys.argv[2])
 logger.info('Path to host file: {}'.format(hosts_path))
 
-new_line = '{}'.format(os.environ.get('YELPCAMP_HOST'))
-logger.info(new_line)
+public_ip = ''
+with open(sys.argv[1], 'r') as f:
+    for line in f:
+        if re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", line):
+            public_ip = line.strip()
+        else:
+            logger.error('No public_ip found in "/jenkins_tmp/ip.txt"')
+            sys.exit(5)
+        logger.info(public_ip)
 
 with open(hosts_path, 'r') as f:
     lines = [line for line in f]
 
-lines[-1] = new_line
+
+p = re.compile(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
+lines[-1] = p.sub(public_ip, lines[-1])
+
 
 os.remove(hosts_path)
 logger.info('host file removed')
@@ -29,7 +41,6 @@ with open(hosts_path, 'w') as f:
             f.write(line)
 if os.path.exists(hosts_path):
     logger.info('hosts file created')
-
 
 
 
