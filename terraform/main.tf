dbbf93713610b0d46593e6076a9203054f933482
Manuel Rodriguez <manuel.rodriguez@ecs-digital.co.uk>
2019-07-17 08:16:52 +0100

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,25 +11,8 @@ resource "aws_instance" "staging_server" {
   subnet_id              = "subnet-0c8c4704d89a2a892"
   vpc_security_group_ids = ["sg-036e368df116c1235"]
 
-  connection {
-    type        = "ssh"
-    user        = "jenkins"
-    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
-  }
-
   provisioner "local-exec" {
     command = "echo ${aws_instance.staging_server.public_ip} > /jenkins_tmp/ip.txt"
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "jenkins"
-    private_key = "${file("${var.PATH_TO_PRIVATE_KEY}")}"
-  }
-
-  provisioner "file" {
-    source      = "/jenkins_tmp/ip.txt"
-    destination = "/tmp/ip.txt"
   }
 
   tags = {
