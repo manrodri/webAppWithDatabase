6d3c494cdb989988a0afe7abfe89e02a9d280c2c
Manuel Rodriguez <manuel.rodriguez@ecs-digital.co.uk>
2019-07-23 09:46:56 +0100

    adding ec2 instance stop/terminate script

--- stop_instance.py
+++ stop_instance.py
@@ -1,17 +1,20 @@
 import boto3
 import logging
 import sys
+import argparse
 
 FORMAT = '[%(levelname)-2s] %(message)s'
 logging.basicConfig(format=FORMAT, level=20)
 logger = logging.getLogger()
 
-if len(sys.argv) < 2:
-    logger.error('Missing argument: stop or terminate')
-    sys.exit(1)
-if sys.argv[1] != 'stop' or sys.argv[1] != 'terminate':
-    logger.error('valid arguments: ["stop", "terminate"]')
-    sys.exit(1)
+# build parser and parse arguments
+parser = argparse.ArgumentParser(description='command line tool to stop or terminate ec2 instance running previous app version')
+
+parser.add_argument('action', choices=['stop', 'terminate'], help='stop or terminate ec2 instance')
+parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
+parser.add_argument('--debug', action='store_true')
+
+args = parser.parse_args()
 
 #get regions
 ec2_client = boto3.client('ec2')
@@ -23,11 +26,11 @@ for r in regions:
     # stop instance based on existance of tag yelpCamp:true
     instances = ec2.instances.filter(Filters=[{"Name": "tag:yelpCamp", 'Values': ['true']}])
     for instance in instances:
-        if sys.argv[1] == 'stop':
+        if args.action == 'stop':
             logging.info('Stopping instance:  {}'.format(instance.id))
             instance.stop()
             logger.info("Stopped instance: {}".format(instance.id))
-        elif sys.argv[2] == 'terminate':
+        elif args.action == 'terminate':
             logging.info('terminating instance:  {}'.format(instance.id))
             instance.terminate()
             logger.info("Terminated instance: {}".format(instance.id))
